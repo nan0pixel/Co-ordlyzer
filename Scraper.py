e528aa2d5a087a985d8732a5ee7698b009a7dac3
pixel <kaleidia@outlook.com>
2024-11-03 01:35:36 -0800

    updated Scraper.py
    added functionality:
    - $get is no longer called on a specific message - it triggers the bot to sift through channel history
    - scrapes solutions to both solved and unsolved co-ordles
    - outputs found solutions as a message in the channel

--- Scraper.py
+++ Scraper.py
@@ -1,9 +1,9 @@
 import discord
 import re
-from discord.ext import commands
 
 TOKEN = ''
 CHANNEL_ID = 0
+COORDLE = 0
 MESSAGE = ''
 
 intents = discord.Intents.default()
@@ -25,35 +25,46 @@ def getWord(line):
 '''
 Determines whether a co-ordle was solved from the color of the embed
 '''
-def solved(embed):
+def color(embed):
     color = embed.color
     if str(color) == '#78b159': # str(color) returns the hex
-        return True
-    else:
-        return False
-    
+        return 'g'
+    elif str(color) == '#dd2e44':
+        return 'r'
+    return None
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     if message.content.startswith('$get'):
-        if message.reference:
-            reference = await message.channel.fetch_message(message.reference.message_id)
-            if reference.embeds:
-                embed = reference.embeds[0]  # Get the first embed
-                content = embed.description if embed.description else "No description available."
-                line = content.strip().split('\n')[-1]  # Split by lines and take the last one
-                if solved(embed):
-                    # return word
+        solutions = []
+        # Discord might limit rate
+        async for msg in message.channel.history(limit=None): 
+
+            # find messages with embeds
+            if msg.embeds:  
+                embed = msg.embeds[0]
+
+                # green embeds - solved
+                if color(embed) == 'g':
+                    content = embed.description if embed.description else ""
+                    line = content.strip().split('\n')[-1] # get answer from "keyboard"
                     word = getWord(line)
-                    await message.channel.send('The word is: ' + word)
-                else:
-                    await message.channel.send('This co-ordle was not solved')
-            else:
-                await message.channel.send('The referenced message does not contain any embeds')
+                    solutions.append(word)
+                
+                # red embeds - unsolved
+                elif color(embed) == 'r':
+                    # look for "The solution was" message in fields for red embeds
+                    for f in embed.fields:
+                        match = re.search(r'The solution was `(\w+)`', str(f))
+                        if match:
+                            solutions.append(match.group(1))
+        solutions = [s.upper() for s in solutions]
+        if solutions:
+            await message.channel.send("Found solutions: " + ', '.join(solutions))
         else:
-            await message.channel.send('This command is not a reply to any message')
-        
+            await message.channel.send("No solutions found with green or red embeds.")
 
 client.run(TOKEN)
