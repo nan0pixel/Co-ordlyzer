c8f49a8b2cfc347109f8b00e9a038a3ec4123539
pixel <kaleidia@outlook.com>
2024-11-25 18:17:34 -0800

    Update wordlist.py
    - creates directory structure if folders and/or files don't exist
    - streamlined program flow
    - implemented channel-specific wordlist files

--- wordlist.py
+++ wordlist.py
@@ -1,36 +1,22 @@
 '''
-STRUCTURE
+--- DIRECTORY STRUCTURE ---
+wordlist.py
+/storage (created if doesn't exist)
+    timestamps.json: (created if doesn't exist) stores the timestamp (encoded in message ID) 
+    of the last Co-ordle retrieved by channel
+    /wordlists (created if doesn't exist)
+        {channel1ID}.txt
+        {channel2ID}.txt
+        (stores unique words seen so far by channel)
 
-Data Storage:
-- wordlist.txt: stores the unique words seen so far
-- lastRetrieval.json: stores the timestamp (encoded in message ID) of the last Co-ordle
-    retrieved by channel
-
-Functions:
-- getWordlist: loads wordlist.txt
-- getNewWords: gets all words since the last retrieval using timestamp.
-    - getTimestamp: gets timestamp of last Co-ordle retrieved
-    - getCoordles: list of all solved and unsolved Co-ordles from channel since timestamp
-    - loop message list, isSolvedCoordle(message)
-        - if True -> getFound()
-        - if False -> getSolution()
-- updateWordlist: compares the list of new words and the existing wordlist, 
-                  adds new unique words to wordlist.txt
-- updateTimestamp: updates timestamp to that of the most recent Co-ordle retrieved
-
-Execution Flow:
+--- EXECUTION FLOW ---
 ?wordlist
-    - getWordlist()
-    - getNewWords()
-        - getTimestamp()
-    - updateWordlist()
-    - updateLastRetrieval()
-
-Calls:
-- ?wordlist
-
-To-dos:
-- create /storage, wordlist.txt, lastRetrieval.json if doesn't exist
+    1. getTimestamp() - gets channel-specific timestamp of last Co-ordle retrieved
+    2. getCoordles() - gets all Co-ordles from channel history since timestamp
+    3. getSolutions() - gets solutions from Co-ordles (both solved and unsolved)
+    4. updateWordlist() - updates channel-specific wordlist file with any new solutions
+    5. updateTimestamp() - updates channel-specific timestamp to that of the most recent Co-ordle
+    6. output - sends embed summarizing number of new Co-ordles and unique solutions found
 '''
 
 import os
@@ -42,15 +28,16 @@ from dotenv import load_dotenv
 
 # --------- DIRECTORY --------- #
 # folder paths
-STORAGE_FOLDER = os.path.dirname(__file__)
-STORAGE_FOLDER = os.path.join(STORAGE_FOLDER, "storage")
+PROJECT_FOLDER = os.path.dirname(__file__)
+STORAGE_FOLDER = os.path.join(PROJECT_FOLDER, 'storage')
+WORDLISTS_FOLDER = os.path.join(STORAGE_FOLDER, "wordlists")
 
 # file paths
-WORDLIST_FILE = os.path.join(STORAGE_FOLDER, "wordlist.txt")
-LAST_RETRIEVAL_FILE = os.path.join(STORAGE_FOLDER, "lastRetrieval.json")
+LAST_RETRIEVAL_FILE = os.path.join(STORAGE_FOLDER, 'timestamps.json')
 
-# Create storage folder if it doesn't exist
+# Create folders if they don't exist
 os.makedirs(STORAGE_FOLDER, exist_ok=True)
+os.makedirs(WORDLISTS_FOLDER, exist_ok=True)
 
 # --------- CONSTANTS --------- #
 EMBED_GREEN = '#78b159' # solved Co-ordle
@@ -68,28 +55,17 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
 # --------- FUNCTIONS --------- #
-# CURRENT WORDLIST
-def getWordlist():
-    '''
-    Loads wordlist.txt from storage folder
-    '''
-    try:
-        with open(WORDLIST_FILE, "r") as f:
-            # read wordlist, strip newline characters -> return as set
-            return set(f.read().splitlines())
-    except FileNotFoundError:
-        print("wordlist.txt not found")
-        return set()
-
 # NEW WORDS RETRIEVED
 def isSolvedCoordle(message):
     '''
     Determines if a message is a Co-ordle and whether it is solved or unsolved
-
-    Returns:
-    True -> solved
-    False -> unsolved
-    None -> not a Co-ordle
+    
+    Parameter
+        message: Discord message
+    Returns
+        True: is Co-ordle, solved
+        False: is Co-ordle, unsolved
+        None: not a Co-ordle
     '''
     if message.author.id == COORDLE and message.embeds:
         embed = message.embeds[0] # message.embeds returns a list
@@ -103,12 +79,11 @@ async def getCoordles(channel, timestamp):
     '''
     Gets all Co-ordles from specified timestamp (usu. since last retrieval)
 
-    Parameters:
+    Parameters
         channel: Discord channel to fetch messages from
         timestamp: starting timestamp from which to filter messages
-
-    Returns:
-        list of Co-ordles
+    Return
+        coordles: list of Co-ordles
     '''
     coordles = []
     async for message in channel.history(after=discord.Object(id=timestamp), limit = None):
@@ -123,7 +98,6 @@ def loadJson(path):
 
     Parameter
         path: JSON file path
-
     Return
         Loaded JSON data (empty dictionary if file not found or empty)
     '''
@@ -137,27 +111,33 @@ def saveJson(path, data):
     '''
     Saves JSON file
 
-    Parameter:
+    Parameter
         path: JSON file path
         data: data to save
     '''
-    try:
-        with open(path, "w") as f:
-            json.dump(data, f, indent=4)
-    except Exception as e:
-        print(f"Error saving JSON file: {e}")
+    with open(path, "w+") as f:
+        json.dump(data, f, indent=4)
 
 def getTimestamp(channel):
     '''
     Gets timestamp (encoded in message ID) of last Co-ordle retrieval in the channel
+
+    Parameter
+        channel: Discord channel   
+    Return
+        Channel-specific timestamp
     '''
     channelID = channel.id
-    lastRetrieval = loadJson(LAST_RETRIEVAL_FILE)
-    return lastRetrieval.get(str(channelID), 0) # return 0 if no timestamp found
+    timestamp = loadJson(LAST_RETRIEVAL_FILE)
+    return timestamp.get(str(channelID), 0) # return 0 if no timestamp found
 
 def updateTimestamp(channel, coordles):
     '''
-    Updates timestamp (encoded in message ID) of last Co-ordle retrieval in the channel
+    Updates timestamp (encoded in message ID) of last Co-ordle retrieval in channel
+
+    Parameters
+        channel: Discord channel
+        coordles: list of coordles
     '''
     if coordles:
         newest = coordles[-1].id  # message ID of most recent Co-ordle
@@ -168,9 +148,14 @@ def updateTimestamp(channel, coordles):
     else:
         print("No new Co-ordles retrieved")
 
-def getFound(coordle):
+def getSolved(coordle):
     '''
     Gets solution from solved Co-ordle
+
+    Parameter
+        coordle: message containing solved Co-ordle 
+    Return
+        word: solution
     '''
     embed = coordle.embeds[0]
     description = embed.description or ""
@@ -179,9 +164,14 @@ def getFound(coordle):
     word = ''.join(letters)
     return word
 
-def getSolution(coordle):
+def getUnsolved(coordle):
     '''
     Gets solution from unsolved Co-ordle
+
+    Parameter
+        coordle: message containing unsolved Co-ordle
+    Return
+        Solution
     '''
     embed = coordle.embeds[0]
     solution = embed.fields[-2].value # custom to Co-ordle structure
@@ -190,20 +180,61 @@ def getSolution(coordle):
         return word.group(1)
     raise ValueError("Solution not found")
 
-async def getNewWords(channel):
+def getWordlist(channelID):
     '''
-    Gets all new words since the last Co-ordle retrieval and updates timestamp
+    Loads channel-specific wordlist file from storage folder
+
+    Parameter
+        channelID: channel ID
+    Return
+        Wordlist as list
     '''
-    words = []
-    timestamp = getTimestamp(channel)
-    coordles = await getCoordles(channel, timestamp)
+    wordlistFile = os.path.join(WORDLISTS_FOLDER, f"{channelID}.txt")
+    try:
+        with open(wordlistFile, "r") as f:
+            # strip newline characters -> return as list
+            return [line.strip() for line in f]
+    except FileNotFoundError:
+        print(f"{channelID} has no existing wordlist")
+        return []
+    
+def updateWordlist(channel, words):
+    '''
+    Updates channel-specific wordlist file with any unique solutions found
+
+    Parameters
+        channel: Discord channel
+        words: list of words from new Co-ordles (not necessarily unique)
+    Return
+        numUnique: number of unique solutions found in new batch of Co-ordles
+    '''
+    channelID = channel.id
+    wordlist = getWordlist(channelID)
+    combined = set(wordlist + words)  # add new words
+    numUnique = len(combined) - len(wordlist)
+
+    wordlistFile = os.path.join(WORDLISTS_FOLDER, f"{channelID}.txt")
+    with open(wordlistFile, 'w+') as f:
+        f.write("\n".join(sorted(combined))) 
+    return numUnique
+
+def getSolutions(coordles):
+    '''
+    Gets solutions from solved and unsolved Co-ordles
+
+    Parameter
+        coordles: list of Co-ordle embeds
+    Return
+        solutions: list of solutions
+    '''
+    solutions = []
     for coordle in coordles:
         if isSolvedCoordle(coordle):
-            words.append(getFound(coordle))
+            solutions.append(getSolved(coordle))
         else:
-            words.append(getSolution(coordle))
-    updateTimestamp(channel, coordles)
-    return words
+            solutions.append(getUnsolved(coordle))
+    solutions = [word.upper() for word in solutions]
+    return solutions
 
 # --------- EXECUTION --------- #
 @bot.event
@@ -214,10 +245,22 @@ async def on_ready():
 @bot.command(name='wordlist')
 async def wordlist(ctx):
     channel = ctx.channel
-    wordlist = getWordlist()
-    newWords = await getNewWords(channel)
+    timestamp = getTimestamp(channel)
+    coordles = await getCoordles(channel, timestamp)
+    solutions = getSolutions(coordles)
+    updateTimestamp(channel, coordles)
+    numUnique = updateWordlist(channel, solutions)
 
-    #updateWordlist(new_words)
-    #updateLastRetrieval(lastRetrieval)
+    # OUTPUT
+    embed = discord.Embed(
+        title = "Wordlist Summary",
+        description = (
+            f"Found `{len(coordles)}` Co-ordle(s) since the last `?wordlist` call, "
+            f"`{numUnique}` of which contained a yet unseen solution."
+        ),
+        color=discord.Color.purple()
+    )
+    embed.timestamp = ctx.message.created_at
+    await ctx.send(embed=embed)
 
 bot.run(TOKEN)
